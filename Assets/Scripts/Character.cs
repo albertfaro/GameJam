07023e8afeb4167edcc0f8f60081bf465fdc4a5d
Albert <albert.faro@enti.cat>
2021-04-12 14:18:07 +0200

    Basic character movement & prefab

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+
+    public int speed = 2;
+    public float rotation = 100;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,40 @@ public class Character : MonoBehaviour
     void Update()
     {
         
+        //MOVEMENT
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, -Input.GetAxis("Horizontal") *
+            rotation * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, Input.GetAxis("Horizontal") *
+            -rotation * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+            {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
+        //...
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //MORDER A GENTE
+        }
+
+
+
     }
 }
+

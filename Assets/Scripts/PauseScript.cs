15d08e592ac7181a897e80e75d22b11c721ff5ee
Albert <albert.faro@enti.cat>
2021-04-12 20:43:40 +0200

    Menú de pausa

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public bool isPaused;
+
+    public GameObject pauseMenuCanvas;
+
+
+    private void Update()
+    {
+        if(isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit(0);
+    }
+}

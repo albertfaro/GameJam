f86f2eeaa183b2fbdfed8a8a24598ede6d71602d
Albert <albert.faro@enti.cat>
2021-04-12 17:38:43 +0200

    Basic Main Menu (No graphics)

---
+++ Assets/Scripts/OptionMenuInactive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenuInactive : MonoBehaviour
+
+{
+
+    public GameObject settingsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void setVolume (float value)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20); //Hay un bug de Unity donde el slider de volumen no funciona correctamente, haciéndolo de este modo el slider funciona de forma lineal
+    }
+}

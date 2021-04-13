0e13802eca4cc86f967899a9efd3c3934d54caed
Albert <albert.faro@enti.cat>
2021-04-13 18:31:44 +0200

    Camera follows player/Camera boundaries

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]    //Los boundaries del mapa
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Playable");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+            );
+        
+
+    }
+}
